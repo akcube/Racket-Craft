7df9a1d46f1475fafb87ae617efa35c50dc99bc7
Abhijnan Vegi <abhijnan.vegi@gmail.com>
2023-01-21 18:08:59 +0530

    Finished uniquify

--- compiler.rkt
+++ compiler.rkt
@@ -9,9 +9,22 @@
 (require "utilities.rkt")
 (provide (all-defined-out))
 
+
+(define (uniquify_exp env)
+  (lambda (e) 
+  (match e
+    [(Var x) (Var (dict-ref env x))] ;;TODO:
+    [(Int n) (Int n)]
+    [(Let x e body) (let ([n (gensym x)]) (Let n ((uniquify_exp env) e) ((uniquify_exp (dict-set env x n)) body)) )] ;;TODO:
+    [(Prim op es) (Prim op (for/list ([e es]) ((uniquify_exp env) e)))]
+  ))
+)
 ;; uniquify : R1 -> R1
 (define (uniquify p)
-  (error "TODO: code goes here (uniquify)"))
+  ; (error "TODO: code goes here (uniquify)"))
+  (match p 
+    [(Program '() e) (Program '() ((uniquify_exp '()) e))])
+)
 
 ;; remove-complex-opera* : R1 -> R1
 (define (remove-complex-opera* p)
@@ -27,7 +40,7 @@
 (define compiler-passes
   `( 
      ;; Uncomment the following passes as you finish them.
-     ;; ("uniquify" ,uniquify ,interp-Lvar ,type-check-Lvar)
+     ("uniquify" ,uniquify ,interp-Lvar ,type-check-Lvar)
      ;; ("remove complex opera*" ,remove-complex-opera* ,interp-Lvar ,type-check-Lvar)
      ;; ("explicate control" ,explicate-control ,interp-Cvar ,type-check-Cvar)
      ))
